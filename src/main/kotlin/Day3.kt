0a7dfcc9cd90daf248b6959075ee1cd9f96d82a4
anilbanarsee <anil.banarsee@googlemail.com>
2022-12-03 23:30:19 +0000

    Refactor Day3 to use kotlin .intersect()
    See https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/intersect.html

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -5,16 +5,15 @@ class Day3 {
 
         private fun result(input: List<String>): Results = Results().apply {
             part1 = input.map { it.half().toList() }
-                .flatMap { it.map { str -> str.toCharArray().toSet() }.getMatchingElements() }
+                .flatMap { it.map { str -> str.toCharArray().toSet() }.intersect() }
                 .sumOf { it.asInt() }
 
            part2 = input.chunked(3)
-                .flatMap { it.map { str -> str.toCharArray().toSet() }.getMatchingElements() }
+                .flatMap { it.map { str -> str.toCharArray().toSet() }.intersect() }
                 .sumOf { it.asInt() }
         }
 
-        private fun <T> Iterable<Set<T>>.getMatchingElements(): Iterable<T> =
-            this.reduce { item, next -> item.filter { next.contains(it) }.toSet() }
+        private fun <T> Iterable<Set<T>>.intersect(): Iterable<T> = this.reduce { item, next -> item.intersect(next) }
 
         private fun String.half(): Pair<String, String> = this.take(this.length / 2) to this.drop(this.length / 2)
 
