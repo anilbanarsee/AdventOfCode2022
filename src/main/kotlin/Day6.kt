dccfaa83bb5151dc39bb824c0cc4f4965d9a3d0f
abanarsee <anil.banarsee@fivium.co.uk>
2022-12-06 11:38:50 +0000

    Day 6 - Clean

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -4,8 +4,8 @@ class Day6 {
         fun main(args: Array<String>) = result(readAsStream(args[0])!!.toList()).run(::println)
 
         private fun result(input: List<String>): Results = Results().apply {
-            part1 = input[0].windowed(4).indexOfFirst { it.toSet().size == 4 } + 4
-            part2 = input[0].windowed(14).indexOfFirst { it.toSet().size == 14 } + 14
+            part1 = 4.let { window -> input[0].windowed(window).indexOfFirst { it.toSet().size == window } + window }
+            part2 = 14.let { window -> input[0].windowed(window).indexOfFirst { it.toSet().size == window } + window }
         }
     }
 }
